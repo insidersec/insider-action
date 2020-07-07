6b802340bde4fe834b081de9a961e95992b3c6c5
Matheus Alcantara <matheusssilv97@gmail.com>
2020-07-07 09:55:30 -0300

    feat: Upload artifacts

--- src/run.ts
+++ src/run.ts
@@ -1,6 +1,8 @@
-import * as path from 'path'
 import * as core from '@actions/core'
 import * as exec from '@actions/exec'
+import * as glob from '@actions/glob'
+import * as artifact from '@actions/artifact'
+import * as path from 'path'
 import * as installer from './installer'
 
 export async function run(): Promise<void> {
@@ -17,9 +19,37 @@ export async function run(): Promise<void> {
     core.info('🏃 Running Insider...')
     await exec.exec(`${insider}`, args)
     core.info(' Finished Insider')
+    uploadArtifacts(insiderPath)
   } catch (error) {
     core.setFailed(error.message)
   }
+}
+
+async function uploadArtifacts(rootDirectory: string): Promise<void> {
+  const artifactClient = artifact.create()
+  const artifactName = 'insider-artifact'
+
+  const files = await getReportFiles()
+  const uploadResponse = await artifactClient.uploadArtifact(
+    artifactName,
+    files,
+    rootDirectory,
+    {continueOnError: false}
+  )
+
+  if (uploadResponse.failedItems.length > 0) {
+    throw new Error(`Error to upload artifacts: ${uploadResponse.failedItems}`)
+  }
+  core.info(
+    `祝 Upload report files with successfull: ${uploadResponse.artifactItems}`
+  )
+}
+
+async function getReportFiles(): Promise<string[]> {
+  const patterns = ['report-*.html', 'report-*.json']
+  const globber = await glob.create(patterns.join('\n'))
+  const files = await globber.glob()
+  return files
 }
 
 function getArguments(): string[] {
@@ -30,7 +60,7 @@ function getArguments(): string[] {
   githubWorkspacePath = path.resolve(githubWorkspacePath)
   core.debug(`GITHUB_WORKSPACE = '${githubWorkspacePath}'`)
 
-  const technology = core.getInput('technology')
+  const technology = core.getInput('technology') || 'javascript' // TODO change this!
   const target = core.getInput('target') || '.'
   const security = core.getInput('security')
   const noJson = core.getInput('noJson')
